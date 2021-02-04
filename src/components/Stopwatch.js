ea17d4cc40aa72887aa5d4e1eccddd87d0f30e9b
rheannemcintosh <rheanne.mcintosh@abdn.ac.uk>
2021-02-04 22:30:57 +0000

    Handle the reset of the stopwatch

--- src/components/Stopwatch.js
+++ src/components/Stopwatch.js
@@ -30,16 +30,25 @@ class Stopwatch extends Component {
             this.setState({ previousTime: Date.now() });
         }
     }
+
+    handleRest = () => {
+        this.setStae({ elapsedTime: 0 });
+    }
     
     render() {
+
+        const seconds = Math.floor(this.state.elapsedTime / 1000);
+
         return (
             <div className="stopwatch">
                 <h2>Stopwatch</h2>
-                <span className="stopwatch-time">0</span>
+                <span className="stopwatch-time">
+                    { seconds }
+                </span>
                 <button onClick={ this.handleStopwatch }>
                     { this.state.isRunning ? 'Stop' : 'Start' }
                 </button>
-                <button>Reset</button>
+                <button onClick={this.handleReset}>Reset</button>
             </div>
         );
     }
