ba79fef63c472b485476a32b84eb653e7fe7f1de
rheannemcintosh <rheanne.mcintosh@abdn.ac.uk>
2021-02-05 22:41:06 +0000

    Add high score crown icon

--- src/components/App.js
+++ src/components/App.js
@@ -61,7 +61,19 @@ class App extends Component {
     });
   }
 
+  getHighScore = () => {
+    const scores = this.state.players.map( p => p.score );
+    const highScore = Math.max(...scores);
+    if (highScore) {
+      return highScore;
+    }
+    return null;
+  }
+
   render() {
+
+    const highScore = this.getHighScore();
+
     return (
       <div className="scoreboard">
         <Header 
@@ -78,7 +90,8 @@ class App extends Component {
             key={player.id.toString()}
             index={index}
             changeScore={this.handleScoreChange}
-            removePlayer={this.handleRemovePlayer}           
+            removePlayer={this.handleRemovePlayer}
+            isHighScore={highScore === player.score}          
           />
         )}
 
