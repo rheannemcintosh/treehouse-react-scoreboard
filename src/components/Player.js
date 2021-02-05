0954b46f7adb1c020bc250c9335f5aceccfb6562
rheannemcintosh <rheanne.mcintosh@abdn.ac.uk>
2021-02-05 22:06:25 +0000

    Destructuring props

--- src/components/Player.js
+++ src/components/Player.js
@@ -2,20 +2,29 @@ import React from 'react';
 import Counter from './Counter';
 
 const Player = (props) => {
-    return (
-        <div className="player">
-            <span className="player-name">
-                <button className="remove-player" onClick={() => props.removePlayer(props.id)}>✖</button>
-                { props.name }
-            </span>
+        const { 
+            name,
+            id,
+            score,
+            index,
+            removePlayer,
+            changeScore
+        } = this.props;
+        return (
+            <div className="player">
+                <span className="player-name">
+                    <button className="remove-player" onClick={() => removePlayer(id)}>✖</button>
+                    { name }
+                </span>
 
-            <Counter 
-                score={props.score}
-                index={props.index}
-                changeScore={props.changeScore} 
-            />
-        </div>
-    );
+                <Counter 
+                    score={score}
+                    index={index}
+                    changeScore={changeScore} 
+                />
+            </div>
+        );
+    }
 }
 
 export default Player;
